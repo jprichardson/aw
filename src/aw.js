5740ab4be23953372854a77630e633e3148ee371
JP Richardson <jprichardson@gmail.com>
2016-05-12 15:33:27 -0500

    aw: simplified

--- src/aw.js
+++ src/aw.js
@@ -1,48 +1,24 @@
-var Promise = require('bluebird')
-var once = require('once')
-
-export function awp (fn, _this) {
+function aw (fn, context) {
   return async function (...args) {
     try {
-      var result = await fn.apply(_this, args)
-      return [result]
+      var result = await wrap(fn, args, context)
+      if (Array.isArray(result)) return [null, ...result]
+      else return [null, result]
     } catch (err) {
-      return [undefined, err]
+      return [err]
     }
   }
 }
 
-export function awc (fn, _this) {
-  return async function (...args) {
-    try {
-      var promiseFn = Promise.promisify(catchify(fn, _this), _this)
-      var result = await promiseFn.apply(_this, args)
-      return [result]
-    } catch (err) {
-      return [undefined, err]
+function wrap (fn, args, context) {
+  return new Promise(function (resolve, reject) {
+    args.push(function injectedAWCallbackArg (err, ...cbArgs) { err == null ? resolve(cbArgs) : reject(err) })
+
+    let p = fn.apply(context, args)
+    if (p instanceof Promise) {
+      p.then(resolve).catch(reject)
     }
-  }
+  })
 }
 
-export function awf (arr) {
-  return [].concat.apply([], arr)
-}
-
-export function catchify (fn, _this) {
-  return function (...args) {
-    var callback = args[args.length - 1]
-    callback = typeof callback === 'function'
-      ? once(callback)
-      : Function()
-
-    var finish = once(function () {
-      process.removeListener('uncaughtException', finish)
-      callback.apply(_this, arguments)
-    })
-
-    args[args.length - 1] = finish
-    process.once('uncaughtException', finish)
-
-    fn.apply(_this, args)
-  }
-}
+module.exports = aw
