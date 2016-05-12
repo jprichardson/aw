502e29847439f1a2009790f84c7a683f6582d040
JP Richardson <jprichardson@gmail.com>
2016-05-12 15:46:19 -0500

    aw: fix sync return val

--- tests/sync.test.js
+++ tests/sync.test.js
@@ -1,7 +1,7 @@
 import test from 'tape-promise/tape'
 import aw from '../src/aw'
 
-test('aw: when error is thrown it should return result', async function (t) {
+test('> when error is thrown it should return result', async function (t) {
   t.plan(3)
 
   function fetchMessage (name) {
@@ -13,6 +13,22 @@ test('aw: when error is thrown it should return result', async function (t) {
   t.ok(err instanceof Error, 'err is error')
   t.is(err.message, 'another error', 'proper error message')
   t.is(typeof res, 'undefined', 'no error result')
+
+  t.end()
+})
+
+test('> when sync it should return result', async function (t) {
+  t.plan(2)
+
+  function fetchMessage (name) {
+    return 'hello ' + name
+  }
+
+  const fetchMessageAwait = aw(fetchMessage)
+  const [err, res] = await fetchMessageAwait('jp')
+
+  t.is(res, 'hello jp', 'result is set')
+  t.notOk(err, 'error is falsy')
 
   t.end()
 })
