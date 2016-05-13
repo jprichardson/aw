bc14a937fb7851a09952d132f964fd346ccd734a
JP Richardson <jprichardson@gmail.com>
2016-05-13 09:11:24 -0500

    tests: async created

---
+++ tests/async.test.js
@@ -0,0 +1,34 @@
+import test from 'tape-promise/tape'
+import aw from '../src/aw'
+
+test('> when error is thrown it should return result', async function (t) {
+  t.plan(3)
+
+  async function fetchMessage (name) {
+    throw new Error('another error')
+  }
+  const fetchMessageAwait = aw(fetchMessage)
+
+  let [err, res] = await fetchMessageAwait('jp')
+  t.ok(err instanceof Error, 'err is error')
+  t.is(err.message, 'another error', 'proper error message')
+  t.is(typeof res, 'undefined', 'no error result')
+
+  t.end()
+})
+
+test('> when sync it should return result', async function (t) {
+  t.plan(2)
+
+  async function fetchMessage (name) {
+    return 'hello ' + name
+  }
+
+  const fetchMessageAwait = aw(fetchMessage)
+  const [err, res] = await fetchMessageAwait('jp')
+
+  t.is(res, 'hello jp', 'result is set')
+  t.notOk(err, 'error is falsy')
+
+  t.end()
+})
