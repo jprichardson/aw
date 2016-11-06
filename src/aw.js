98566519b61b152f408edca4931fe7263ed756c9
Kirill Fomichev <fanatid@ya.ru>
2016-11-06 13:59:10 +0300

    injectCallback is false by default

--- src/aw.js
+++ src/aw.js
@@ -1,53 +1,50 @@
 const isFunc = (fn) => typeof fn === 'function'
 const isObj = (obj) => obj != null && typeof obj === 'object'
 
-function aw (...args) {
-  if (args.length === 2) {
-    if (isFunc(args[0]) && isObj(args[1])) return _aw(args[0], args[1])
-    if (isObj(args[0]) && isFunc(args[1])) return _aw(args[1], args[0])
-    throw new Error('aw uknown types.')
-  } else if (args.length === 1) {
-    if (isFunc(args[0])) return _aw(args[0])
-    if (isObj(args[0])) return (fn) => _aw(fn, args[0])
-    throw new Error('aw unknown type.')
-  }
-  throw new Error('aw incorrect number of parameters')
-}
-
-function _aw (fn, options = {}) {
-  return async function (...args) {
-    const opts = { context: this, injectCallback: true, catch: true, ...options }
-    if (opts.catch) {
-      try {
-        let { type, val } = await wrap(fn, args, opts)
-        if (type === 'cb' && Array.isArray(val)) return [null, ...val]
-        else return [null, val]
-      } catch (err) {
-        return [err]
-      }
-    } else {
-      let { type, val } = await wrap(fn, args, opts)
-      if (type === 'cb' && Array.isArray(val)) return [...val]
-      else return val
-    }
-  }
-}
-
 function wrap (fn, args, options) {
-  return new Promise(function (resolve, reject) {
+  return new Promise((resolve, reject) => {
     if (options.injectCallback) {
-      args.push(function injectedAWCallbackArg (err, ...cbArgs) { err == null ? resolve({ type: 'cb', val: cbArgs }) : reject(err) })
+      args.push(function injectedAWCallbackArg (err, ...cbArgs) {
+        err == null ? resolve(cbArgs) : reject(err)
+      })
     }
 
-    let p = fn.apply(options.context, args)
-    if (p instanceof Promise) {
-      p.then(function (val) {
-        resolve({ type: 'p', val })
-      }).catch(reject)
-    } else if (p) {
-      resolve({ type: 's', val: p })
-    }
+    // call fn in Promise, because fn can be synchronous functions and throw error
+    const p = fn.apply(options.context, args)
+    if (!options.injectCallback) return Promise.resolve(p).then(resolve, reject)
   })
+}
+
+function _aw (fn, options) {
+  options = Object.assign({ injectCallback: false, catch: true }, options)
+
+  return async function (...args) {
+    const opts = Object.assign({ context: this }, options)
+    if (!opts.catch) return wrap(fn, args, opts)
+
+    return wrap(fn, args, opts)
+      .then((val) => {
+        if (opts.injectCallback && Array.isArray(val)) return [null, ...val]
+        else return [null, val]
+      }, (err) => [err])
+  }
+}
+
+function aw (...args) {
+  switch (args.length) {
+    case 1:
+      if (isFunc(args[0])) return _aw(args[0])
+      if (isObj(args[0])) return (fn) => _aw(fn, args[0])
+      throw new Error('aw unknown type.')
+
+    case 2:
+      if (isFunc(args[0]) && isObj(args[1])) return _aw(args[0], args[1])
+      if (isObj(args[0]) && isFunc(args[1])) return _aw(args[1], args[0])
+      throw new Error('aw uknown types.')
+
+    default:
+      throw new Error('aw incorrect number of parameters.')
+  }
 }
 
 module.exports = aw
